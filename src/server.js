4366f122c7f8a481b61ae9b447237607f5651eee
Lucas-dev23 <Ldelimacardoso.ll@gmail.com>
2026-01-01 19:57:59 -0300

    feat: conexao com postgres

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import app from './app.js';
+import "dotenv/config";
 
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
